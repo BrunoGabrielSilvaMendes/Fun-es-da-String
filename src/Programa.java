3949be46f3e8fc1ce96bf8ab7f498e1088b24079
Bruno Gabriel <brunobmx@gmail.com>
2023-03-22 17:14:58 -0300

    segundo programa

--- src/Programa.java
+++ src/Programa.java
@@ -1,28 +1,23 @@
+import java.util.Scanner;
 
 public class Programa {
     public static void main(String[] args) {
 
-        String original = "abcde FGHIJ ABC abc DEFG   ";
-        String s01 = original.toUpperCase();
-        String s02 = original.toLowerCase();
-        String s03 = original.trim();
-        String s04 = original.substring(2,9);
-        String s05 = original.replace('a', 'x');
-        String s06 = original.replace("abc", "xy");
+        Scanner sc = new Scanner(System.in);
 
-        int i = original.indexOf("bc");
-        int j = original.lastIndexOf("bc");
+        System.out.println("Qual número é o maior ?");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.println("Original = -" + original + "-");
-        System.out.println("Upper Case = -" +s01 + "-");
-        System.out.println("Lower Case = -" + s02 + "-");
-        System.out.println("Trim = -" + s03 + "-");
-        System.out.println("Substring = -" + s04 + "-");
-        System.out.println("Replace = -" + s05 + "-");
-        System.out.println("Replace = -" + s06 + "-");
-        System.out.println("IndexOf = " + i);
-        System.out.println("lastIndexOf = " + j);
+        if (a > b && a > c) {
+            System.out.println("Número maior = A");
+        } else if (b > c) {
+            System.out.println("Número maior = B");
+        } else
+            System.out.println("Número maior = C");
 
+        sc.close();
 
     }
 }
